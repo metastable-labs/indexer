1ef7409da81557ea0cc32071a8b72ad3d648e9a3
Tele Falola <oluwateledemi@gmail.com>
2024-12-12 05:36:43 +0100

    chore: log env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('APPLE_BUNDLE_ID:', process.env.APPLE_BUNDLE_ID);
+  console.log('APPLE_TEAM_ID:', process.env.APPLE_TEAM_ID);
+  console.log('RP_ID:', process.env.RP_ID);
+  console.log('RP_NAME:', process.env.RP_NAME);
   await app.listen(3006);
 }
 bootstrap();
