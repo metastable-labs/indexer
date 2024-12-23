a6b38c7253fae78caae49b2e0c52ea17b99a216a
Tele Falola <oluwateledemi@gmail.com>
2024-12-23 13:46:25 +0100

    chore: remove aave events

--- ponder.config.ts
+++ ponder.config.ts
@@ -3,7 +3,6 @@ import { http } from "viem";
 import { StrategyContractAbi } from "./abis/StrategyContractAbi";
 import { EngineContractAbi } from "./abis/EngineContractAbi";
 import config from "./src/config";
-import { AaveContractAbi } from "./abis/AaveContractAbi";
 
 const appConfig = config();
 export default createConfig({
@@ -31,11 +30,5 @@ export default createConfig({
       address: "0xa1f8E7DD401ad584F689710a81DF9F7f3056ca6C",
       startBlock: 23911733,
     },
-    // AaveContract: {
-    //   network: "mainnet",
-    //   abi: AaveContractAbi,
-    //   address: "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5",
-    //   startBlock: 23911733,
-    // },
   },
 });
